461bedf3755e4d35b4e29945bd2f60a518976c02
David Dume <dumedavid11@gmail.com>
2023-03-14 15:08:22 +0100

    aggiunta immagine

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Ciao Mondo'
+      message: 'Ciao Mondo',
+      img: 'https://images.unsplash.com/photo-1678719510338-f37390296951?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80'
     }
   }
 }).mount('#app')
